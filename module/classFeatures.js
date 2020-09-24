bca97855f5fac8085aeb4d999a75e98b167aad54
Matteo <matteopieroni6@gmail.com>
2020-09-24 19:41:17 +0100

    Added wanderer and hunter of shadows progression

--- module/classFeatures.js
+++ module/classFeatures.js
@@ -1,66 +1,66 @@
 export const ClassFeatures = {
- "scholar": {
-   "subclasses": {
-     "master-healer": {
-       "label": "Master Healer",
-       "source": "MAN pg. 67"
-     },
-     "master-scholar": {
-       "label": "Master Scholar",
-       "source": "MAN pg. 68",
-       "features": {
-         "3": ["Compendium.dnd5e.aimeclassfeatures.lxnRr36pLYTWF24L", "Compendium.dnd5e.aimeclassfeatures.okw3z2icLNrjzdXU"],
-         "14": ["Compendium.dnd5e.aimeclassfeatures.e91MKtVimeynNoRF"],
-         "18": ["Compendium.dnd5e.aimeclassfeatures.ndkGGrNRKRoR0w0O"],
-       }
-     },
-   },
-   "features": {
-     "1": ["Compendium.dnd5e.aimeclassfeatures.IbwpRN4WVei4Igda", "Compendium.dnd5e.aimeclassfeatures.EHOUkUotFWt28ORO", "Compendium.dnd5e.aimeclassfeatures.VLAqoExKCuBnL75k"],
-     "2": ["Compendium.dnd5e.aimeclassfeatures.2i666pPq0PSmUHWd"],
-     "3": ["Compendium.dnd5e.aimeclassfeatures.UWrSkLrYNhLPbrjg"],
-     "4": ["Compendium.dnd5e.aimeclassfeatures.Xe4WZ4iZerKSf5Qw"],
-     "5": ["Compendium.dnd5e.aimeclassfeatures.PGMm9d1y4BDjarT8"],
-		 "6": ["Compendium.dnd5e.aimeclassfeatures.jraNKgmjpD2XLsQZ"],
-		 "9": ["Compendium.dnd5e.aimeclassfeatures.xlHZN2ruVbirQ2zY"],
-		 "11": ["Compendium.dnd5e.aimeclassfeatures.jHAbQE7xSeyc2MYc"],
-		 "13": ["Compendium.dnd5e.aimeclassfeatures.7HQW0irvBzvgh6Pj"],
-		 "15": ["Compendium.dnd5e.aimeclassfeatures.s6KUf5BEY27C5hmV"],
-		 "17": ["Compendium.dnd5e.aimeclassfeatures.Hme8PSjpH41ge9b4"],
-		 "16": ["Compendium.dnd5e.aimeclassfeatures.Xe4WZ4iZerKSf5Qw"],
-		 "20": ["Compendium.dnd5e.aimeclassfeatures.418WOxbRYulUr8HD"],
-   }
- },
- "slayer": {
-   "subclasses": {
-     "the-rider": {
-       "label": "The Rider",
-       "source": "MAN pg. 74",
-			 "features": {}
-     },
-     "foe-hammer": {
-       "label": "Foe Hammer",
-       "source": "MAN pg. 75",
-			 "features": {
-				 "3": ["Compendium.dnd5e.aimeclassfeatures.dP8WnJWfLIq0KaIP"],
-				 "10": ["Compendium.dnd5e.aimeclassfeatures.q8l7QVfhryMGlbZA"],
-				 "14": ["Compendium.dnd5e.aimeclassfeatures.QJ5NaAN8hjPxrSuU"],
-			 }
-     },
-   },
-   "features": {
-		 "1": ["Compendium.dnd5e.aimeclassfeatures.DsiaNvtLzVigb17h", "Compendium.dnd5e.aimeclassfeatures.YVvlB2F7lzBMq0Mq"],
-		 "2": ["Compendium.dnd5e.aimeclassfeatures.9UbrfYy42IVfEcsq"],
-		 "3": ["Compendium.dnd5e.aimeclassfeatures.llzdUX2V0VinpbNd"],
-		 "4": ["Compendium.dnd5e.aimeclassfeatures.Xe4WZ4iZerKSf5Qw"],
-		 "5": ["Compendium.dnd5e.aimeclassfeatures.Mr547RJPwIHxUtuG", "Compendium.dnd5e.aimeclassfeatures.RM9Dd7w1MNbsTJtj"],
-		 "6": ["Compendium.dnd5e.aimeclassfeatures.dm0OSmzgpDMN4gIR"],
-		 "7": ["Compendium.dnd5e.aimeclassfeatures.i39hxfE6sHLaDtoB"],
-		 "9": ["Compendium.dnd5e.aimeclassfeatures.OEooOrIQHZYMyuPh"],
-		 "11": ["Compendium.dnd5e.aimeclassfeatures.iIFlQOZEdH7whI09"],
-		 "15": ["Compendium.dnd5e.aimeclassfeatures.ysVWT3fgmxk6L5WI"],
-		 "18": ["Compendium.dnd5e.aimeclassfeatures.vIg3DB41tDsvFZ8H"],
-		 "20": ["Compendium.dnd5e.aimeclassfeatures.nRzeDuES9KbPD5Nh"],
+	"scholar": {
+		"subclasses": {
+			"master-healer": {
+				"label": "Master Healer",
+				"source": "MAN pg. 67"
+			},
+			"master-scholar": {
+				"label": "Master Scholar",
+				"source": "MAN pg. 68",
+				"features": {
+					"3": ["Compendium.dnd5e.aimeclassfeatures.lxnRr36pLYTWF24L", "Compendium.dnd5e.aimeclassfeatures.okw3z2icLNrjzdXU"],
+					"14": ["Compendium.dnd5e.aimeclassfeatures.e91MKtVimeynNoRF"],
+					"18": ["Compendium.dnd5e.aimeclassfeatures.ndkGGrNRKRoR0w0O"],
+				}
+			},
+		},
+		"features": {
+			"1": ["Compendium.dnd5e.aimeclassfeatures.IbwpRN4WVei4Igda", "Compendium.dnd5e.aimeclassfeatures.EHOUkUotFWt28ORO", "Compendium.dnd5e.aimeclassfeatures.VLAqoExKCuBnL75k"],
+			"2": ["Compendium.dnd5e.aimeclassfeatures.2i666pPq0PSmUHWd"],
+			"3": ["Compendium.dnd5e.aimeclassfeatures.UWrSkLrYNhLPbrjg"],
+			"4": ["Compendium.dnd5e.aimeclassfeatures.Xe4WZ4iZerKSf5Qw"],
+			"5": ["Compendium.dnd5e.aimeclassfeatures.PGMm9d1y4BDjarT8"],
+			"6": ["Compendium.dnd5e.aimeclassfeatures.jraNKgmjpD2XLsQZ"],
+			"9": ["Compendium.dnd5e.aimeclassfeatures.xlHZN2ruVbirQ2zY"],
+			"11": ["Compendium.dnd5e.aimeclassfeatures.jHAbQE7xSeyc2MYc"],
+			"13": ["Compendium.dnd5e.aimeclassfeatures.7HQW0irvBzvgh6Pj"],
+			"15": ["Compendium.dnd5e.aimeclassfeatures.s6KUf5BEY27C5hmV"],
+			"17": ["Compendium.dnd5e.aimeclassfeatures.Hme8PSjpH41ge9b4"],
+			"16": ["Compendium.dnd5e.aimeclassfeatures.Xe4WZ4iZerKSf5Qw"],
+			"20": ["Compendium.dnd5e.aimeclassfeatures.418WOxbRYulUr8HD"],
+		}
+	},
+	"slayer": {
+		"subclasses": {
+			"the-rider": {
+				"label": "The Rider",
+				"source": "MAN pg. 74",
+				"features": {}
+			},
+			"foe-hammer": {
+				"label": "Foe Hammer",
+				"source": "MAN pg. 75",
+				"features": {
+					"3": ["Compendium.dnd5e.aimeclassfeatures.dP8WnJWfLIq0KaIP"],
+					"10": ["Compendium.dnd5e.aimeclassfeatures.q8l7QVfhryMGlbZA"],
+					"14": ["Compendium.dnd5e.aimeclassfeatures.QJ5NaAN8hjPxrSuU"],
+				}
+			},
+		},
+		"features": {
+			"1": ["Compendium.dnd5e.aimeclassfeatures.DsiaNvtLzVigb17h", "Compendium.dnd5e.aimeclassfeatures.YVvlB2F7lzBMq0Mq"],
+			"2": ["Compendium.dnd5e.aimeclassfeatures.9UbrfYy42IVfEcsq"],
+			"3": ["Compendium.dnd5e.aimeclassfeatures.llzdUX2V0VinpbNd"],
+			"4": ["Compendium.dnd5e.aimeclassfeatures.Xe4WZ4iZerKSf5Qw"],
+			"5": ["Compendium.dnd5e.aimeclassfeatures.Mr547RJPwIHxUtuG", "Compendium.dnd5e.aimeclassfeatures.RM9Dd7w1MNbsTJtj"],
+			"6": ["Compendium.dnd5e.aimeclassfeatures.dm0OSmzgpDMN4gIR"],
+			"7": ["Compendium.dnd5e.aimeclassfeatures.i39hxfE6sHLaDtoB"],
+			"9": ["Compendium.dnd5e.aimeclassfeatures.OEooOrIQHZYMyuPh"],
+			"11": ["Compendium.dnd5e.aimeclassfeatures.iIFlQOZEdH7whI09"],
+			"15": ["Compendium.dnd5e.aimeclassfeatures.ysVWT3fgmxk6L5WI"],
+			"18": ["Compendium.dnd5e.aimeclassfeatures.vIg3DB41tDsvFZ8H"],
+			"20": ["Compendium.dnd5e.aimeclassfeatures.nRzeDuES9KbPD5Nh"],
 		}
 	},
 	"treasure-hunter": {
@@ -94,55 +94,74 @@ export const ClassFeatures = {
 			"18": ["Compendium.dnd5e.aimeclassfeatures.fyMtuEFYHHWUe695"],
 			"20": ["Compendium.dnd5e.aimeclassfeatures.QTZu57zOmWbxYpzz"],
 		},
- },
- "wanderer": {
-   "subclasses": {
-     "hunter-of-beasts": {
-       "label": "Hunter of Beasts",
-       "source": "MAN pg. 84",
-       "features": {}
-     },
-     "hunter-of-shadows": {
-       "label": "Hunter of Shadows",
-       "source": "MAN pg. 85",
-       "features": {}
-     },
-  	},
-   "features": {},
- },
- "warden": {
-   "subclasses": {
-     "counsellor": {
-       "label": "Counsellor",
-       "source": "MAN pg. 88",
-       "features": {}
-     },
-     "herald": {
-       "label": "Herald",
-       "source": "MAN pg. 90",
-       "features": {}
-     },
-     "bounder": {
-       "label": "Bounder",
-       "source": "MAN pg. 90",
-       "features": {}
-     }
-   },
-   "features": {},
- },
- "warrior": {
-   "subclasses": {
-     "knight": {
-       "label": "Knight",
-       "source": "MAN pg. 93",
-       "features": {}
-     },
-     "weapon-master": {
-       "label": "Weapon Master",
-       "source": "MAN pg. 95",
-       "features": {}
-     }
-   },
-   "features": {},
- }
+	},
+	"wanderer": {
+		"subclasses": {
+			"hunter-of-beasts": {
+				"label": "Hunter of Beasts",
+				"source": "MAN pg. 84",
+				"features": {}
+			},
+			"hunter-of-shadows": {
+				"label": "Hunter of Shadows",
+				"source": "MAN pg. 85",
+				"features": {
+					"3": ["Compendium.dnd5e.aimeclassfeatures.aPxjbce1Imav43vw"],
+					"7": ["Compendium.dnd5e.aimeclassfeatures.nGIFYB9MS3FmXqxv"],
+					"11": ["Compendium.dnd5e.aimeclassfeatures.G9Zmk1rXdjAhZJCC"],
+					"15": ["Compendium.dnd5e.aimeclassfeatures.802uIDHCr7242exQ"],
+				}
+			},
+		},
+		"features": {
+			"1": ["Compendium.dnd5e.aimeclassfeatures.exUuCOY5wbZJZeXA", "Compendium.dnd5e.aimeclassfeatures.nOTWIePukKMN68l2"],
+			"2": ["Compendium.dnd5e.aimeclassfeatures.5Xp2oNnmcAjAmP2E", "Compendium.dnd5e.aimeclassfeatures.FEPjBVSzszQuaIqj", "Compendium.dnd5e.aimeclassfeatures.H7W7v3OqL3VmjqYK", "Compendium.dnd5e.aimeclassfeatures.ad9JUYdEohcL5OY0", "Compendium.dnd5e.aimeclassfeatures.hudpvfnbC9JXJk7q"],
+			"3": ["Compendium.dnd5e.aimeclassfeatures.p2Fb0H5iZ5D2pG7T", "Compendium.dnd5e.aimeclassfeatures.2dArMlzvQqJk5Kk2"],
+			"4": ["Compendium.dnd5e.aimeclassfeatures.Xe4WZ4iZerKSf5Qw"],
+			"5": ["Compendium.dnd5e.aimeclassfeatures.Mr547RJPwIHxUtuG"],
+			"8": ["Compendium.dnd5e.aimeclassfeatures.ceKvWDsStneNCGkZ"],
+			"9": ["Compendium.dnd5e.aimeclassfeatures.UsoPoHIbHNYRHTTQ"],
+			"10": ["Compendium.dnd5e.aimeclassfeatures.WpxIQocFhmuCD9Vy"],
+			"13": ["Compendium.dnd5e.aimeclassfeatures.0q0vdgicbQ5NHOlf"],
+			"14": ["Compendium.dnd5e.aimeclassfeatures.ep1lXV6z1OxVo1II"],
+			"17": ["Compendium.dnd5e.aimeclassfeatures.l7xEEcn3eW7riHDH"],
+			"18": ["Compendium.dnd5e.aimeclassfeatures.fVc3V3klFGO5PJ5A"],
+			"20": ["Compendium.dnd5e.aimeclassfeatures.2jT68Zdb2X1vHFpH"],
+		},
+	},
+	"warden": {
+		"subclasses": {
+			"counsellor": {
+				"label": "Counsellor",
+				"source": "MAN pg. 88",
+				"features": {}
+			},
+			"herald": {
+				"label": "Herald",
+				"source": "MAN pg. 90",
+				"features": {}
+			},
+			"bounder": {
+				"label": "Bounder",
+				"source": "MAN pg. 90",
+				"features": {}
+			}
+		},
+		"features": {},
+	},
+	"warrior": {
+		"subclasses": {
+			"knight": {
+				"label": "Knight",
+				"source": "MAN pg. 93",
+				"features": {}
+			},
+			"weapon-master": {
+				"label": "Weapon Master",
+				"source": "MAN pg. 95",
+				"features": {}
+			}
+		},
+		"features": {},
+	}
 };
