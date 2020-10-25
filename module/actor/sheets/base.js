75e5fe2fb0b8ae74af9c93dfc471ceb065738b73
Matteo Pieroni <matteopieroni6@gmail.com>
2020-10-25 17:05:05 +0000

    [Bugfix] Skills error (#5)
    * Fixed a small bug with skills breaking

--- module/actor/sheets/base.js
+++ module/actor/sheets/base.js
@@ -91,7 +91,7 @@ export default class ActorSheet5e extends ActorSheet {
         skl.ability = CONFIG.DND5E.abilityAbbreviations[skl.ability];
         skl.icon = this._getProficiencyIcon(skl.value);
         skl.hover = CONFIG.DND5E.proficiencyLevels[skl.value];
-        skl.label = CONFIG.DND5E.skills[s];
+        skl.label = CONFIG.DND5E.skills[s] || s;
       }
     }
 
